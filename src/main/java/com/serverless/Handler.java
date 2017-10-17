486cf9bceb4392dd82315c725c2ff1b420cf2d90
Takafumi Yoshida <zephiransas@gmail.com>
2017-10-17 15:18:48 +0900

    Implement write query params to DynamoDB

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -1,8 +1,15 @@
 package com.serverless;
 
-import java.util.Collections;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import org.apache.log4j.Logger;
 
 import com.amazonaws.services.lambda.runtime.Context;
@@ -15,11 +22,32 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
 		LOG.info("received: " + input);
+
+		writeDynamoDB(input);
+
 		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
 				.setObjectBody(responseBody)
 				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
 				.build();
+	}
+
+	private void writeDynamoDB(Map<String, Object> input) {
+		AmazonDynamoDB client = AmazonDynamoDBClientBuilder
+				.standard()
+				.withRegion(Regions.AP_NORTHEAST_1)
+				.build();
+
+		DynamoDB dynamoDB = new DynamoDB(client);
+		Table table = dynamoDB.getTable(System.getenv("DYNAMODB_TABLE"));
+
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
+		Item item = new Item()
+				.withPrimaryKey("id", UUID.randomUUID().toString())
+				.with("created_at", format.format(new Date()));
+		input.forEach((key, value) -> item.with(key, value));
+
+		table.putItem(item);
 	}
 }
